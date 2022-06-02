2b885c5419c1289c48a154077e2b87663f48fa2d
“julisa” <milagrosjulisa.mm@gmail.com>
2022-06-01 21:26:46 -0500

    Base de Spring Boot

---
+++ src/main/java/com/java/entelgy/EntelgyApplication.java
@@ -0,0 +1,13 @@
+package com.java.entelgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntelgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntelgyApplication.class, args);
+	}
+
+}
